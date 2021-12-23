f3ce977e7c2079feacaadc1cc3bbcb6a40e9e7b6
SENIOR\Guilherme.Moreira <guilherme.moreira@senior.com.br>
2021-12-23 16:49:18 -0300

    Transmite dados informados no output para parágrafos em tela

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -8,6 +8,8 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent{
 
+  @Output() onTransferir = new EventEmitter<any>();
+
   valor: number;
   destino: number;
 
@@ -15,5 +17,15 @@ export class NovaTransferenciaComponent{
     console.log('Solicitada nova transferência');
     console.log('Valor:', this.valor);
     console.log('Destino:', this.destino);
+
+    const valorEmitir = { valor: this.valor, destino: this.destino }
+    this.onTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
